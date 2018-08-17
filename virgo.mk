8d99c2dca931c3153a3d85f1d9dda3a562659253
guaiyihu <guaiyihu@foxmail.com>
2018-08-17 12:02:14 +0800

    virgo: Drop unused bl_lut.txt
    Change-Id: I7e75c149aa52826434a2718aa8802dbccaaf725a

--- virgo.mk
+++ virgo.mk
@@ -139,9 +139,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/MTP/MTP_Speaker_cal.acdb:system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb
 
 # Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/bl_lut.txt:system/etc/bl_lut.txt
-
 PRODUCT_PACKAGES += \
     bdaddr_xiaomi
 

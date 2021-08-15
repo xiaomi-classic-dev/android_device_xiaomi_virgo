253eec5670db165f4ea8a9b04b4100163b6e5ce3
GHF0127 <64727004+GHF0127@users.noreply.github.com>
2021-08-15 23:41:31 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_virgo.mk
+    $(LOCAL_DIR)/cherish_virgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_virgo-user \
-    lineage_virgo-userdebug \
-    lineage_virgo-eng
+    cherish_virgo-user \
+    cherish_virgo-userdebug \
+    cherish_virgo-eng
